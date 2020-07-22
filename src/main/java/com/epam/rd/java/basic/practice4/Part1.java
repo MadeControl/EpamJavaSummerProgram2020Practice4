93b0f4f632b02e0ab8b978c503db202370199a85
MadeControl <danya.polishchuk.2001@ukr.ent>
2020-07-22 11:51:33 +0300

    added part1

--- src/main/java/com/epam/rd/java/basic/practice4/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part1.java
@@ -4,6 +4,18 @@ public class Part1 {
 
     public static void main(String[] args) {
 
+        if(!(args == null)){
+            for(String str : args){
+                if(str.length() > 4){
+                    System.out.print(str.substring(str.length()-(str.length() - 2)) + " ");
+                } else {
+                    System.out.print(str + " ");
+                }
+            }
+        } else {
+            throw new NullPointerException();
+        }
+
     }
 
 }
--- src/test/java/com/epam/rd/java/basic/practice4/Part1Test.java
+++ src/test/java/com/epam/rd/java/basic/practice4/Part1Test.java
@@ -1,5 +1,16 @@
 package com.epam.rd.java.basic.practice4;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Part1Test {
+
+    @Test
+    public void mainTest(){
+
+        final String[] args = {"qwerty", "qwer"};
+        Part1.main(args);
+
+    }
 
 }
