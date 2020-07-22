f6f65d1da028c7703df818f10e546751b4956eb0
MadeControl <danya.polishchuk.2001@ukr.ent>
2020-07-22 13:10:11 +0300

    added part1

--- src/main/java/com/epam/rd/java/basic/practice4/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part1.java
@@ -1,20 +1,49 @@
 package com.epam.rd.java.basic.practice4;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 public class Part1 {
 
     public static void main(String[] args) {
+        BufferedReader br;
+        StringBuilder sb = new StringBuilder();
 
-        if(!(args == null)){
-            for(String str : args){
-                if(str.length() > 4){
-                    System.out.print(str.substring(str.length()-(str.length() - 2)) + " ");
-                } else {
-                    System.out.print(str + " ");
-                }
+        try {
+
+            br = new BufferedReader(new InputStreamReader(new FileInputStream("part1.txt"), StandardCharsets.UTF_8));
+
+            String temp;
+            while ( (temp = br.readLine()) != null){
+                sb.append(temp).append(System.lineSeparator());
             }
-        } else {
-            throw new NullPointerException();
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        
+        String[] stringsOfContent = sb.toString().split("\n");
+        sb = new StringBuilder();
+
+        for(String str : stringsOfContent){
+
+            String[] wordsOfString = str.split("[\\s\n\r]");
+
+            for (String word : wordsOfString){
+
+                if(word.length() >= 4){
+                    sb.append(word.substring(word.length() - (word.length() - 2)));
+                } else {
+                    sb.append(word);
+                } sb.append(" ");
+
+            } sb.append("\n");
+
+        }
+        System.out.print(sb.delete(sb.length()-2, sb.length()).toString());
 
     }
 
