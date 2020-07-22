31307c9bc201316064f616854d0df15cd31a4a61
MadeControl <danya.polishchuk.2001@ukr.ent>
2020-07-22 19:29:40 +0300

    added part3

--- src/main/java/com/epam/rd/java/basic/practice4/Part3.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part3.java
@@ -1,9 +1,65 @@
 package com.epam.rd.java.basic.practice4;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Part3 {
 
+    public static final String REGEX_INT = "(?<!\\.)\\b\\d+\\b(?!\\.)";
+    public static final String REGEX_DOUBLE = "\\d*\\.\\d+";
+    public static final String REGEX_CHAR = "\\b\\w\\b";
+    public static final String REGEX_STRING = "\\b[\\w&&\\D]{2,}\\b";
+    public static final String FILE_CONTENT = Part1.readFromFile("part3.txt", "cp1251");
+    private static final Scanner SCANNER = new Scanner(System.in);
+
     public static void main(String[] args) {
+        String inputData;
+        String result;
+
+        while (true){
+            inputData = SCANNER.nextLine();
+            result = getResultOperation(inputData);
+
+            if(result.equals("stop")){
+                break;
+            } else if(result.equals("")) {
+                continue;
+            } System.out.println(result);
+        }
 
     }
+
+    public static String getValuesFromFile(String regEx){
+
+        Pattern pattern = Pattern.compile(regEx, Pattern.UNICODE_CHARACTER_CLASS);
+        Matcher matcher = pattern.matcher(FILE_CONTENT);
+        StringBuilder stringBuilder = new StringBuilder();
+        while (matcher.find()){
+            stringBuilder.append(matcher.group()).append(" ");
+        } return stringBuilder.delete(stringBuilder.length()-1, stringBuilder.length()).toString();
+
+    }
+
+    public static String getResultOperation(String inputData){
+
+        switch (inputData){
+            case "char":
+                return getValuesFromFile(REGEX_CHAR);
+            case "int":
+                return getValuesFromFile(REGEX_INT);
+            case "String":
+                return getValuesFromFile(REGEX_STRING);
+            case "double":
+                return getValuesFromFile(REGEX_DOUBLE);
+            case "stop":
+                return "stop";
+            default: return "";
+        }
+
+    }
+
+
+
 
 }
--- src/test/java/com/epam/rd/java/basic/practice4/Part3Test.java
+++ src/test/java/com/epam/rd/java/basic/practice4/Part3Test.java
@@ -1,5 +1,27 @@
 package com.epam.rd.java.basic.practice4;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Part3Test {
+
+    @Test
+    public void getValuesFromFileTest(){
+
+        final String expectedString = "432 89";
+
+        Assert.assertEquals(expectedString, Part3.getValuesFromFile(Part3.REGEX_INT));
+
+    }
+
+    @Test
+    public void getResultOperationTest(){
+
+        final String expectedString = "43.43 .98";
+        final String inputData = "double";
+
+        Assert.assertEquals(expectedString, Part3.getResultOperation(inputData));
+
+    }
 
 }
