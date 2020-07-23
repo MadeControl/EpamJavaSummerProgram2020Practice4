1f1c25c1aca0feb2de038f222138233e57650710
MadeControl <danya.polishchuk.2001@ukr.ent>
2020-07-23 12:47:17 +0300

    fixed demo

--- src/main/java/com/epam/rd/java/basic/practice4/Demo.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Demo.java
@@ -9,31 +9,32 @@ public class Demo {
     private static final InputStream STD_IN = System.in;
 
     public static void main(String[] args) {
+        final String[] arguments = {};
 
         System.out.println("=========================== PART1");
-        Part1.main(args);
+        Part1.main(arguments);
 
         System.out.println("=========================== PART2");
-        Part2.main(args);
+        Part2.main(arguments);
 
         System.out.println("=========================== PART3");
         // set the mock input
         System.setIn(new ByteArrayInputStream("char^String^int^double^stop"
                 .replace("^", System.lineSeparator()).getBytes(StandardCharsets.UTF_8)));
 
-        Part3.main(args);
+        Part3.main(arguments);
         // restore the standard input
         System.setIn(STD_IN);
 
         System.out.println("=========================== PART4");
-        Part4.main(args);
+        Part4.main(arguments);
 
         System.out.println("=========================== PART5");
         // set the mock input
         System.setIn(new ByteArrayInputStream("table ru^table en^apple ru^stop"
                 .replace("^", System.lineSeparator()).getBytes(StandardCharsets.UTF_8)));
 
-        Part5.main(args);
+        Part5.main(arguments);
         // restore the standard input
         System.setIn(STD_IN);
 
@@ -42,7 +43,7 @@ public class Demo {
         System.setIn(new ByteArrayInputStream("Latn^Cyrl^asdf^latn^cyrl^stop"
                 .replace("^", System.lineSeparator()).getBytes(StandardCharsets.UTF_8)));
 
-        Part6.main(args);
+        Part6.main(arguments);
         // restore the standard input
         System.setIn(STD_IN);
 
